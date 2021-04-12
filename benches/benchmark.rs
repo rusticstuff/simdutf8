cf6a7ad75098a7b9045ac070c6509aefc32af75b
Hans Kratz <hans@appfour.com>
2021-04-12 16:01:47 +0200

    rename benchmarks for critcmp

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -17,15 +17,19 @@ fn bench(c: &mut Criterion, name: &str, bytes: &[u8]) {
         let slice = get_valid_slice_of_len_or_more(bytes, *i);
         group.throughput(Throughput::Bytes(slice.len() as u64));
         group.bench_with_input(
-            BenchmarkId::new("simd", slice.len()),
+            BenchmarkId::new("simd", format!("{:05}", slice.len())),
             &slice,
             |b, &slice| {
                 b.iter(|| validate_utf8(slice).unwrap());
             },
         );
-        group.bench_with_input(BenchmarkId::new("std", slice.len()), &slice, |b, &slice| {
-            b.iter(|| std::str::from_utf8(slice).unwrap());
-        });
+        group.bench_with_input(
+            BenchmarkId::new("std", format!("{:05}", slice.len())),
+            &slice,
+            |b, &slice| {
+                b.iter(|| std::str::from_utf8(slice).unwrap());
+            },
+        );
     }
     group.finish();
 }
@@ -33,18 +37,26 @@ fn bench(c: &mut Criterion, name: &str, bytes: &[u8]) {
 fn criterion_benchmark(c: &mut Criterion) {
     let core_ids = core_affinity::get_core_ids().unwrap();
     core_affinity::set_for_current(*core_ids.get(2).unwrap_or(&core_ids[0]));
-    bench(c, "latin", include_str!("text/Latin-Lipsum.txt").as_bytes());
     bench(
         c,
-        "cyrillic",
+        "1-latin",
+        include_str!("text/Latin-Lipsum.txt").as_bytes(),
+    );
+    bench(
+        c,
+        "2-cyrillic",
         include_str!("text/Russian-Lipsum.txt").as_bytes(),
     );
     bench(
         c,
-        "chinese",
+        "3-chinese",
         include_str!("text/Chinese-Lipsum.txt").as_bytes(),
     );
-    bench(c, "emoij", include_str!("text/Emoji-Lipsum.txt").as_bytes());
+    bench(
+        c,
+        "4-emoij",
+        include_str!("text/Emoji-Lipsum.txt").as_bytes(),
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
