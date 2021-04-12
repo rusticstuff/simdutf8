44b0d00a488a06ab48e46c36da0756755824cdd9
Hans Kratz <hans@appfour.com>
2021-04-12 19:23:59 +0200

    add benchmark for empty string

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,4 +1,7 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::measurement::WallTime;
+use criterion::{
+    criterion_group, criterion_main, BenchmarkGroup, BenchmarkId, Criterion, Throughput,
+};
 use simdutf8::*;
 
 fn get_valid_slice_of_len_or_more(s: &[u8], len: usize) -> &[u8] {
@@ -15,28 +18,39 @@ fn bench(c: &mut Criterion, name: &str, bytes: &[u8]) {
     let mut group = c.benchmark_group(name);
     for i in [1, 8, 64, 512, 4096, 65536].iter() {
         let slice = get_valid_slice_of_len_or_more(bytes, *i);
-        group.throughput(Throughput::Bytes(slice.len() as u64));
-        group.bench_with_input(
-            BenchmarkId::new("simd", format!("{:05}", slice.len())),
-            &slice,
-            |b, &slice| {
-                b.iter(|| validate_utf8(slice).unwrap());
-            },
-        );
-        group.bench_with_input(
-            BenchmarkId::new("std", format!("{:05}", slice.len())),
-            &slice,
-            |b, &slice| {
-                b.iter(|| std::str::from_utf8(slice).unwrap());
-            },
-        );
+        bench_input(&mut group, slice, true);
     }
     group.finish();
+}
+
+fn bench_input(group: &mut BenchmarkGroup<WallTime>, input: &[u8], with_throughput: bool) {
+    if with_throughput {
+        group.throughput(Throughput::Bytes(input.len() as u64));
+    }
+    group.bench_with_input(
+        BenchmarkId::new("simd", format!("{:05}", input.len())),
+        &input,
+        |b, &slice| {
+            b.iter(|| validate_utf8(slice).unwrap());
+        },
+    );
+    group.bench_with_input(
+        BenchmarkId::new("std", format!("{:05}", input.len())),
+        &input,
+        |b, &slice| {
+            b.iter(|| std::str::from_utf8(slice).unwrap());
+        },
+    );
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let core_ids = core_affinity::get_core_ids().unwrap();
     core_affinity::set_for_current(*core_ids.get(2).unwrap_or(&core_ids[0]));
+
+    let mut group = c.benchmark_group("0-empty");
+    bench_input(&mut group, b"", false);
+    group.finish();
+
     bench(
         c,
         "1-latin",
