4299e00f7d5f892630d114d105aa272fd59fa095
Hans Kratz <hans@appfour.com>
2021-04-14 07:50:00 +0200

    restructure implementation modules

---
+++ src/implementation/mod.rs
@@ -0,0 +1,5 @@
+#[allow(dead_code)]
+pub(crate) mod avx2;
+
+#[allow(dead_code)]
+pub(crate) mod sse42;
--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,7 @@
 mod macros;
 mod utf8check;
 
-mod avx2;
-mod sse42;
+mod implementation;
 
 // #[cfg(all(
 //     not(feature = "allow-non-simd"),
@@ -43,14 +42,14 @@ pub fn validate_utf8(input: &[u8]) -> std::result::Result<&str, Utf8Error> {
     #[allow(unused_unsafe)]
     unsafe {
         #[cfg(feature = "force-avx2")]
-        return avx2::implementation::validate_utf8_simd(input);
+        return implementation::avx2::validate_utf8_simd(input);
         #[cfg(feature = "force-sse42")]
-        return sse42::implementation::validate_utf8_simd(input);
+        return implementation::sse42::validate_utf8_simd(input);
         #[cfg(not(any(feature = "force-avx2", feature = "force-sse42")))]
         if is_x86_feature_detected!("avx2") {
-            avx2::implementation::validate_utf8_simd(input)
+            implementation::avx2::validate_utf8_simd(input)
         } else if is_x86_feature_detected!("sse4.2") {
-            sse42::implementation::validate_utf8_simd(input)
+            implementation::sse42::validate_utf8_simd(input)
         } else {
             std::str::from_utf8(input).map_err(|_| Utf8Error {})
         }
