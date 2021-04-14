4256371a5430ac91db8692cf9c1156d93d2e4dc9
Hans Kratz <hans@appfour.com>
2021-04-14 08:13:44 +0200

    cleanup

--- src/implementation/utf8check.rs
+++ src/implementation/utf8check.rs
@@ -1,20 +1,3 @@
-/*
- * legal utf-8 byte sequence
- * http://www.unicode.org/versions/Unicode6.0.0/ch03.pdf - page 94
- *
- *  Code Points        1st       2s       3s       4s
- * U+0000..U+007F     00..7F
- * U+0080..U+07FF     C2..DF   80..BF
- * U+0800..U+0FFF     E0       A0..BF   80..BF
- * U+1000..U+CFFF     E1..EC   80..BF   80..BF
- * U+D000..U+D7FF     ED       80..9F   80..BF
- * U+E000..U+FFFF     EE..EF   80..BF   80..BF
- * U+10000..U+3FFFF   F0       90..BF   80..BF   80..BF
- * U+40000..U+FFFFF   F1..F3   80..BF   80..BF   80..BF
- * U+100000..U+10FFFF F4       80..8F   80..BF   80..BF
- *
- */
-
 pub(crate) struct Utf8CheckingState<T> {
     pub prev: T,
     pub incomplete: T,
