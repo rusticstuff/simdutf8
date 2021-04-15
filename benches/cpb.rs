b0717b3ed5a078393a744d351dc0535d45820be9
Hans Kratz <hans@appfour.com>
2021-04-15 12:47:56 +0200

    refactor out cpb benchmark

--- benches/cpb.rs
+++ benches/cpb.rs
@@ -1,11 +1,11 @@
-use criterion::measurement::WallTime;
 use criterion::{criterion_group, criterion_main, Criterion};
+use criterion_cycles_per_byte::CyclesPerByte;
 
 mod common;
 
 criterion_group!(
     name = benches;
-    config = Criterion::default();
+    config = Criterion::default().with_measurement(CyclesPerByte);
     targets = common::criterion_benchmark
 );
 criterion_main!(benches);
