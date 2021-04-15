80fe47d343151a6d74ea732927ad713687727772
Hans Kratz <hans@appfour.com>
2021-04-15 11:17:22 +0200

    add from_utf8_mut()

--- src/lib.rs
+++ src/lib.rs
@@ -29,5 +29,16 @@ pub fn from_utf8(input: &[u8]) -> core::result::Result<&str, Utf8Error> {
     }
 }
 
+/// Checks if the byte sequence is valid UTF-8 and returns `Ok(str)` if it is.
+///
+/// # Errors
+/// Will return `Err(Utf8Error)` on if the input contains invalid UTF-8
+pub fn from_utf8_mut(input: &mut [u8]) -> core::result::Result<&mut str, Utf8Error> {
+    unsafe {
+        implementation::validate_utf8(input)?;
+        Ok(core::str::from_utf8_unchecked_mut(input))
+    }
+}
+
 #[cfg(test)]
 mod tests;
