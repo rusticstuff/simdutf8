fb75722bc621e13e3e51ab3c5c0b578fa2037770
Hans Kratz <hans@appfour.com>
2021-04-18 09:13:30 +0200

    simdjson library bindings for benchmarking

---
+++ bench/simdjson-utf8/build.rs
@@ -0,0 +1,82 @@
+use std::{env, path::PathBuf, process::Command};
+
+fn get_cpp_link_stdlib() -> Option<String> {
+    if let Ok(stdlib) = env::var("CXXSTDLIB") {
+        if stdlib.is_empty() {
+            None
+        } else {
+            Some(stdlib)
+        }
+    } else {
+        let target = env::var("TARGET").unwrap();
+        if target.contains("msvc") {
+            None
+        } else if target.contains("apple") {
+            Some("c++".to_string())
+        } else if target.contains("freebsd") {
+            Some("c++".to_string())
+        } else if target.contains("openbsd") {
+            Some("c++".to_string())
+        } else {
+            Some("stdc++".to_string())
+        }
+    }
+}
+
+fn main() {
+    let version = "0.9.2";
+    let download_url =
+        "https://github.com/simdjson/simdjson/archive/refs/tags/v".to_owned() + version + ".tar.gz";
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    eprintln!("download");
+    assert!(Command::new("curl")
+        .args(&["-L", "-o", "simdjson.tar.gz", &download_url])
+        .current_dir(&out_path)
+        .status()
+        .unwrap()
+        .success());
+    eprintln!("download done");
+
+    eprintln!("extracting");
+    Command::new("tar")
+        .args(&["xzf", "simdjson.tar.gz"])
+        .current_dir(&out_path)
+        .status()
+        .unwrap();
+    eprintln!("extracting done");
+
+    eprintln!("building");
+    let simdjson_dir = out_path.as_os_str().to_str().unwrap().to_owned() + "/simdjson-" + version;
+    let dst = cmake::Config::new(&simdjson_dir)
+        .define("SIMDJSON_JUST_LIBRARY", "ON")
+        .define("SIMDJSON_BUILD_STATIC", "ON")
+        .uses_cxx11()
+        .build();
+    eprintln!("building done");
+
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=simdjson");
+
+    if let Some(stdlib) = get_cpp_link_stdlib() {
+        println!("cargo:rustc-link-lib={}", stdlib);
+    }
+
+    eprintln!("bindings");
+    let bindings = bindgen::Builder::default()
+        .clang_args(&[
+            "-xc++",
+            "-std=c++17",
+            &("-I".to_owned() + &simdjson_dir + "/include"),
+        ])
+        .header(&(simdjson_dir.to_owned() + "/include/simdjson/implementation.h"))
+        .whitelist_function("simdjson::validate_utf8")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("unable to generate bindings");
+    eprintln!("bindings done");
+
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ bench/simdjson-utf8/src/lib.rs
@@ -0,0 +1,28 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+use std::os::raw::c_char;
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[inline(always)]  // to match the really_inline on the underlying simdjson fn
+pub fn validate(bytes: &[u8]) -> bool {
+    unsafe { simdjson_validate_utf8(bytes.as_ptr() as *const c_char, bytes.len() as size_t) }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::validate;
+
+    #[test]
+    fn valid_utf8() {
+        let hello = "hello world!";
+        assert!(validate(hello.as_bytes()));
+    }
+
+    #[test]
+    fn invalid_utf8() {
+        let hello = "hello world!";
+        assert!(!validate(hello.as_bytes()));
+    }
+}
