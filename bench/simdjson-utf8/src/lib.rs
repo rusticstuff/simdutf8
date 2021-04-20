919750786b58a5506246103a349b43bd8dc3fe88
Hans Kratz <hans@appfour.com>
2021-04-20 14:45:32 +0200

    fix failing test in simdjson bench integration crate

--- bench/simdjson-utf8/src/lib.rs
+++ bench/simdjson-utf8/src/lib.rs
@@ -5,7 +5,7 @@ use std::os::raw::c_char;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-#[inline(always)]  // to match the really_inline on the underlying simdjson fn
+#[inline(always)] // to match the really_inline on the underlying simdjson fn
 pub fn validate(bytes: &[u8]) -> bool {
     unsafe { simdjson_validate_utf8(bytes.as_ptr() as *const c_char, bytes.len() as size_t) }
 }
@@ -22,7 +22,7 @@ mod tests {
 
     #[test]
     fn invalid_utf8() {
-        let hello = "hello world!";
-        assert!(!validate(hello.as_bytes()));
+        let hello = b"\xff";
+        assert!(!validate(hello));
     }
 }
