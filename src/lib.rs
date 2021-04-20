01910f6ffd8f08449a120b23382260cff0857bb2
Hans Kratz <hans@appfour.com>
2021-04-20 18:27:30 +0200

    (failing) doc test

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 //! UTF-8 checking crate
+//!
+//! Quick start:
+//! ```
+//! use simdutf8::basic::{from_utf8, Utf8Error};
+//!
+//! fn main() {
+//!     println!("{}", from_utf8(b"I \xEE\x80\xA2 UTF-8!").unwrap());
+//! }
+//! ```
 
 pub mod compat;
 mod implementation;
