3b5d373efb97875bd34ccdfb918c4a5d74e012bd
Hans Kratz <hans@appfour.com>
2021-04-21 10:37:50 +0200

    update doc

--- src/compat.rs
+++ src/compat.rs
@@ -3,7 +3,9 @@
 //! In particular, [`from_utf8()`]
 //! returns an [`Utf8Error`], which has the [`valid_up_to()`](Utf8Error#method.valid_up_to) and
 //! [`error_len()`](Utf8Error#method.error_len) methods. The first is useful for verification of streamed data. The
-//! second is useful. It also fails early: errors are checked on-the-fly as the string is processed and once
+//! second is useful e.g. for replacing invalid byte sequences with a replacement character.
+//!
+//! The functions in this module also fail early: errors are checked on-the-fly as the string is processed and once
 //! an invalid UTF-8 sequence is encountered, it returns without processing the rest of the data.
 //! This comes at a performance penality compared to the [`crate::basic`] module even when the input is valid UTF-8.
 
