b492280c93ce19f2c782bc0599a939e3243885e6
Hans Kratz <hans@appfour.com>
2021-04-26 10:07:46 +0200

    update simdjson bench comment

--- bench/simdjson-utf8/src/lib.rs
+++ bench/simdjson-utf8/src/lib.rs
@@ -5,7 +5,7 @@ use std::os::raw::c_char;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-#[inline(always)] // to match the really_inline on the underlying simdjson fn
+#[inline(always)] // only works if simdjson ist compiled with LLVM and cross-language LTO is enabled
 pub fn validate(bytes: &[u8]) -> bool {
     unsafe { simdjson_validate_utf8(bytes.as_ptr() as *const c_char, bytes.len() as size_t) }
 }
