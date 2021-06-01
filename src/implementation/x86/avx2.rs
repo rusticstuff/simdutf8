38b58de365e5be84885635bb2bb2e06ece7bc04e
Hans Kratz <hans@appfour.com>
2021-06-01 14:10:43 +0200

    only run avx2 masked load test if AVX 2 is available

--- src/implementation/x86/avx2.rs
+++ src/implementation/x86/avx2.rs
@@ -334,11 +334,18 @@ unsafe fn simd_prefetch(ptr: *const u8) {
 }
 
 mod test {
+    #[cfg(not(features = "std"))]
+    extern crate std;
+
     #[allow(unused_imports)]
     use super::*;
 
     #[test]
     pub fn masked_load() {
+        if std::is_x86_feature_detected!("avx2") {
+            return;
+        }
+
         let arr = [
             1_u8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
             24, 25, 26, 27, 28, 29, 30, 31, 32,
