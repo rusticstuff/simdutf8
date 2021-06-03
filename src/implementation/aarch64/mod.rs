7e346a8831183dd28d905cf14a8f9ea29e4defc8
Hans Kratz <hans@appfour.com>
2021-06-03 12:51:50 +0200

    Faster than std impl on Apple Silicon

--- src/implementation/aarch64/mod.rs
+++ src/implementation/aarch64/mod.rs
@@ -5,10 +5,6 @@ pub(crate) mod neon;
 #[inline]
 #[cfg(all(feature = "aarch64_neon", target_feature = "neon"))]
 pub(crate) unsafe fn validate_utf8_basic(input: &[u8]) -> Result<(), crate::basic::Utf8Error> {
-    if input.len() < super::helpers::SIMD_CHUNK_SIZE {
-        return super::validate_utf8_basic_fallback(input);
-    }
-
     validate_utf8_basic_neon(input)
 }
 
@@ -24,10 +20,6 @@ pub(crate) use super::validate_utf8_basic_fallback as validate_utf8_basic;
 #[inline]
 #[cfg(all(feature = "aarch64_neon", target_feature = "neon"))]
 pub(crate) unsafe fn validate_utf8_compat(input: &[u8]) -> Result<(), crate::compat::Utf8Error> {
-    if input.len() < super::helpers::SIMD_CHUNK_SIZE {
-        return super::validate_utf8_compat_fallback(input);
-    }
-
     validate_utf8_compat_neon(input)
 }
 
