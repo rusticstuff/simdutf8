44cd43c42f305525127e681718e1e3e6e571c626
Hans Kratz <hans@appfour.com>
2021-06-18 12:45:01 +0200

    benchmark for small inputs of a random length in ranges

---
+++ bench/benches/small_std.rs
@@ -0,0 +1,3 @@
+use simdutf8_bench::define_throughput_benchmark;
+
+define_small_benchmark!(BenchFn::Std);
--- bench/src/macros.rs
+++ bench/src/macros.rs
@@ -8,14 +8,38 @@ macro_rules! define_throughput_benchmark {
 
         use simdutf8_bench::*;
 
-        fn benchmark_compat<M: Measurement>(c: &mut Criterion<M>) {
+        fn benchmark_throughput<M: Measurement>(c: &mut Criterion<M>) {
             criterion_benchmark(c, $bench_fn);
         }
 
         criterion_group!(
             name = benches;
             config = Criterion::default().measurement_time(Duration::from_secs(1)).warm_up_time(Duration::from_secs(1)).sample_size(300);
-            targets = benchmark_compat
+            targets = benchmark_throughput
+        );
+
+        criterion_main!(benches);
+    };
+}
+
+#[macro_export]
+macro_rules! define_small_benchmark {
+    ($bench_fn:expr) => {
+        use std::time::Duration;
+
+        use criterion::measurement::Measurement;
+        use criterion::{criterion_group, criterion_main, Criterion};
+
+        use simdutf8_bench::*;
+
+        fn benchmark_small<M: Measurement>(c: &mut Criterion<M>) {
+            criterion_benchmark_small(c, $bench_fn);
+        }
+
+        criterion_group!(
+            name = benches;
+            config = Criterion::default().measurement_time(Duration::from_secs(1)).warm_up_time(Duration::from_secs(1)).sample_size(300);
+            targets = benchmark_small
         );
 
         criterion_main!(benches);
