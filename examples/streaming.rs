6d4861a196316d94029204eb0032c0e626f560cc
Hans Kratz <hans@appfour.com>
2021-08-31 17:31:45 +0200

    streaming example is only for x86

--- examples/streaming.rs
+++ examples/streaming.rs
@@ -1,11 +1,13 @@
-#[cfg(feature = "public_imp")]
-use simdutf8::basic::imp::Utf8Validator;
-
 #[allow(unused_imports)]
 use std::io::{stdin, Read, Result};
 
-#[cfg(feature = "public_imp")]
+#[cfg(all(
+    feature = "public_imp",
+    any(target_arch = "x86", target_arch = "x86_64")
+))]
 fn main() -> Result<()> {
+    use simdutf8::basic::imp::Utf8Validator;
+
     unsafe {
         if !std::is_x86_feature_detected!("avx2") {
             panic!("This example only works with CPUs supporting AVX 2");
@@ -32,5 +34,8 @@ fn main() -> Result<()> {
 }
 
 /// Dummy main. This example requires the crate feature `public_imp`.
-#[cfg(not(feature = "public_imp"))]
+#[cfg(not(all(
+    feature = "public_imp",
+    any(target_arch = "x86", target_arch = "x86_64")
+)))]
 fn main() {}
