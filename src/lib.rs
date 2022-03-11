49ed8b17b5011800e37870c64413919310ea0b3a
Hans Kratz <hans@appfour.com>
2022-03-11 07:43:39 +0100

    remove stdsimd feature gate for aarch64

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,6 @@
 #![deny(missing_docs)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
-#![cfg_attr(
-    all(feature = "aarch64_neon", target_arch = "aarch64"),
-    feature(stdsimd)
-)]
 
 //! Blazingly fast API-compatible UTF-8 validation for Rust using SIMD extensions, based on the implementation from
 //! [simdjson](https://github.com/simdjson/simdjson). Originally ported to Rust by the developers of [simd-json.rs](https://simd-json.rs), but now heavily improved.
