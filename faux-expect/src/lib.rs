e321cdb046d0d0218bafaa3eb430d7c473014fff
Hans Kratz <hans@appfour.com>
2024-09-24 12:01:31 +0000

    use allow most of the times

--- faux-expect/src/lib.rs
+++ faux-expect/src/lib.rs
@@ -1,15 +1,35 @@
 extern crate proc_macro;
 
-#[rustversion::not(any(since(2024-06-27), since(1.81)))]
+#[rustversion::any(before(1.43.0), all(nightly, before(2020-02-27)))]
 #[proc_macro_attribute]
 pub fn compat_expect(
     _attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
+    // using #[allow(...)] does not work before 1.43.0 due to a bug
     item
 }
 
-#[rustversion::any(since(2024-06-27), since(1.81))]
+#[rustversion::all(
+    any(since(1.43.0), all(nightly, since(2020-02-27))),
+    any(before(1.81), all(nightly, before(2024-06-27))))]
+#[proc_macro_attribute]
+pub fn compat_expect(
+    attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    use proc_macro2::TokenStream;
+    use quote::quote;
+    let attr = TokenStream::from(attr);
+    let item = TokenStream::from(item);
+    let expanded = quote! {
+        #[allow(#attr)]
+        #item
+    };
+    expanded.into()
+}
+
+#[rustversion::any(since(1.81), all(nightly, since(2024-06-27)))]
 #[proc_macro_attribute]
 pub fn compat_expect(
     attr: proc_macro::TokenStream,
