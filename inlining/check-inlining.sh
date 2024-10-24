6038dd9d31b9954ae625b009eb1fa804cf2ece4f
Hans Kratz <hans@appfour.com>
2024-10-24 08:15:27 +0200

    fix for 1.38.0

--- inlining/check-inlining.sh
+++ inlining/check-inlining.sh
@@ -18,7 +18,18 @@ else
     pattern=" (t|T|U) "
     cut_arg=20
 fi
-inline_ignore_pattern='__aeabi_unwind_cpp_pr(0|1)|drop_in_place|core::str::converts::from_utf8|std_detect::detect::|::fmt::|^\$x\.|^<T as core::convert::From<T>>::from$|^core::result::Result<T,E>::map_err$'
+inline_ignore_pattern=\
+'__aeabi_unwind_cpp_pr(0|1)|'\
+'drop_in_place|'\
+'core::str::converts::from_utf8|'\
+'std_detect::detect::|'\
+'::fmt::|'\
+'^\$x\.|'\
+'^<T as core::convert::From<T>>::from$|'\
+'^core::str::Utf8Error::error_len$|'\
+'^core::str::Utf8Error::valid_up_to$|'\
+'^core::str::from_utf8$|'\
+'^core::result::Result<T,E>::map_err$'
 if [[ $target == *wasm* ]]; then
     inline_ignore_pattern="$inline_ignore_pattern|ct_function_table|pointer|r::converts::from_utf8|t::Formatter::write_str|t::write"
 fi
