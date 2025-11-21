02b192d69659adc868e225845b71720d698ddda1
Hans Kratz <hans@appfour.com>
2025-11-21 16:55:44 +0000

    readd TempSimdChunk for public imp

--- src/implementation/x86/avx512.rs
+++ src/implementation/x86/avx512.rs
@@ -260,5 +260,6 @@ unsafe fn simd_prefetch(ptr: *const u8) {
 }
 
 const PREFETCH: bool = true;
+use crate::implementation::helpers::TempSimdChunkA64 as TempSimdChunk;
 simd_input_512_bit!(#[target_feature(enable = "avx512f,avx512bw,avx512vbmi")]);
 algorithm_simd!(#[target_feature(enable = "avx512f,avx512bw,avx512vbmi")]);
