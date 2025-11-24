fccd383895731f02a3493ec59b8a7478af6fbc5c
Hans Kratz <hans@appfour.com>
2025-11-24 12:09:27 +0000

    avx512 public imp

--- src/implementation/x86/mod.rs
+++ src/implementation/x86/mod.rs
@@ -1,15 +1,69 @@
-#[cfg(any(feature = "std", feature = "public_imp", target_feature = "avx2"))]
-pub(crate) mod avx2;
-
-// TODO: require actually necessary AVX-512 features
-#[cfg(any(feature = "std", feature = "public_imp", target_feature = "avx2"))]
+#[cfg(all(
+    avx512_stable,
+    any(
+    feature = "public_imp",
+    // always availabe, except if no-std and no avx512 support
+    feature = "std",
+    all(
+        target_feature = "avx512f",
+        target_feature = "avx512bw",
+        target_feature = "avx512vbmi",
+        target_feature = "avx512vbmi2"
+    )
+)))]
 pub(crate) mod avx512;
 
 #[cfg(any(
     feature = "public_imp",
-    all(feature = "std", not(target_feature = "avx2")),
+    // std: avx2 is available for auto-selection if no avx512 is present
+    all(
+        feature = "std",
+        not(all(
+            avx512_stable,
+            target_feature = "avx512f",
+            target_feature = "avx512bw",
+            target_feature = "avx512vbmi",
+            target_feature = "avx512vbmi2"
+        ))
+    ),
+    // no-std: no avx512 -> select avx2
     all(
         not(feature = "std"),
+        not(all(
+            avx512_stable,
+            target_feature = "avx512f",
+            target_feature = "avx512bw",
+            target_feature = "avx512vbmi",
+            target_feature = "avx512vbmi2"
+        )),
+        target_feature = "avx2"
+    )
+))]
+pub(crate) mod avx2;
+
+#[cfg(any(
+    feature = "public_imp",
+    // std: sse 4.2 is available for auto-selection if no avx512 is present
+    all(
+        feature = "std",
+            not(all(
+                avx512_stable,
+                target_feature = "avx512f",
+                target_feature = "avx512bw",
+                target_feature = "avx512vbmi",
+                target_feature = "avx512vbmi2"
+            )),
+    ),
+    // no-std: no avx512, no avx2 -> select sse4.2
+    all(
+        not(feature = "std"),
+        not(all(
+            avx512_stable,
+            target_feature = "avx512f",
+            target_feature = "avx512bw",
+            target_feature = "avx512vbmi",
+            target_feature = "avx512vbmi2"
+        )),
         not(target_feature = "avx2"),
         target_feature = "sse4.2"
     )
@@ -18,7 +72,17 @@ pub(crate) mod sse42;
 
 // validate_utf8_basic() std: implementation auto-selection
 
-#[cfg(all(feature = "std", not(target_feature = "avx2")))]
+#[cfg(all(
+    feature = "std",
+    not(all(
+        avx512_stable,
+        target_feature = "avx512f",
+        target_feature = "avx512bw",
+        target_feature = "avx512vbmi",
+        target_feature = "avx512vbmi2"
+    )),
+    not(all(not(avx512_stable), target_feature = "avx2"))
+))]
 #[inline]
 pub(crate) unsafe fn validate_utf8_basic(
     input: &[u8],
@@ -30,15 +94,30 @@ pub(crate) unsafe fn validate_utf8_basic(
     type FnRaw = *mut ();
     type ValidateUtf8Fn = unsafe fn(input: &[u8]) -> Result<(), crate::basic::Utf8Error>;
 
+    #[cfg(avx512_stable)]
     #[inline]
-    fn get_fastest_available_implementation_basic() -> ValidateUtf8Fn {
+    fn get_avx512_implementation() -> Option<ValidateUtf8Fn> {
         // Test for avx512vbmi2 to make sure we have a newer CPU with a non-throttling AVX-512 implementation
         if std::is_x86_feature_detected!("avx512f")
             && std::is_x86_feature_detected!("avx512bw")
             && std::is_x86_feature_detected!("avx512vbmi")
             && std::is_x86_feature_detected!("avx512vbmi2")
         {
-            avx512::validate_utf8_basic
+            return Some(avx512::validate_utf8_basic);
+        }
+        None
+    }
+
+    #[cfg(not(avx512_stable))]
+    #[inline]
+    fn get_avx512_implementation() -> Option<ValidateUtf8Fn> {
+        None
+    }
+
+    #[inline]
+    fn get_fastest_available_implementation_basic() -> ValidateUtf8Fn {
+        if let Some(fun) = get_avx512_implementation() {
+            fun
         } else if std::is_x86_feature_detected!("avx2") {
             avx2::validate_utf8_basic
         } else if std::is_x86_feature_detected!("sse4.2") {
@@ -66,7 +145,51 @@ pub(crate) unsafe fn validate_utf8_basic(
 
 // validate_utf8_basic() no-std: implementation selection by config
 
-#[cfg(target_feature = "avx2")]
+#[cfg(all(
+    avx512_stable,
+    target_feature = "avx512f",
+    target_feature = "avx512bw",
+    target_feature = "avx512vbmi",
+    target_feature = "avx512vbmi2"
+))]
+pub(crate) unsafe fn validate_utf8_basic(
+    input: &[u8],
+) -> core::result::Result<(), crate::basic::Utf8Error> {
+    if input.len() < super::helpers::SIMD_CHUNK_SIZE {
+        return super::validate_utf8_basic_fallback(input);
+    }
+
+    validate_utf8_basic_avx512(input)
+}
+
+#[cfg(all(
+    avx512_stable,
+    target_feature = "avx512f",
+    target_feature = "avx512bw",
+    target_feature = "avx512vbmi",
+    target_feature = "avx512vbmi2"
+))]
+#[inline(never)]
+unsafe fn validate_utf8_basic_avx512(
+    input: &[u8],
+) -> core::result::Result<(), crate::basic::Utf8Error> {
+    avx512::validate_utf8_basic(input)
+}
+
+#[cfg(any(
+    all(
+        not(feature = "std"),
+        not(all(
+            avx512_stable,
+            target_feature = "avx512f",
+            target_feature = "avx512bw",
+            target_feature = "avx512vbmi",
+            target_feature = "avx512vbmi2"
+        )),
+        target_feature = "avx2"
+    ),
+    all(target_feature = "avx2", feature = "std", not(avx512_stable))
+))]
 pub(crate) unsafe fn validate_utf8_basic(
     input: &[u8],
 ) -> core::result::Result<(), crate::basic::Utf8Error> {
@@ -77,7 +200,20 @@ pub(crate) unsafe fn validate_utf8_basic(
     validate_utf8_basic_avx2(input)
 }
 
-#[cfg(target_feature = "avx2")]
+#[cfg(any(
+    all(
+        not(feature = "std"),
+        not(all(
+            avx512_stable,
+            target_feature = "avx512f",
+            target_feature = "avx512bw",
+            target_feature = "avx512vbmi",
+            target_feature = "avx512vbmi2"
+        )),
+        target_feature = "avx2"
+    ),
+    all(target_feature = "avx2", feature = "std", not(avx512_stable))
+))]
 #[inline(never)]
 unsafe fn validate_utf8_basic_avx2(
     input: &[u8],
@@ -87,6 +223,13 @@ unsafe fn validate_utf8_basic_avx2(
 
 #[cfg(all(
     not(feature = "std"),
+    not(all(
+        avx512_stable,
+        target_feature = "avx512f",
+        target_feature = "avx512bw",
+        target_feature = "avx512vbmi",
+        target_feature = "avx512vbmi2"
+    )),
     not(target_feature = "avx2"),
     target_feature = "sse4.2"
 ))]
@@ -102,6 +245,13 @@ pub(crate) unsafe fn validate_utf8_basic(
 
 #[cfg(all(
     not(feature = "std"),
+    not(all(
+        avx512_stable,
+        target_feature = "avx512f",
+        target_feature = "avx512bw",
+        target_feature = "avx512vbmi",
+        target_feature = "avx512vbmi2"
+    )),
     not(target_feature = "avx2"),
     target_feature = "sse4.2"
 ))]
@@ -114,6 +264,13 @@ unsafe fn validate_utf8_basic_sse42(
 
 #[cfg(all(
     not(feature = "std"),
+    not(all(
+        avx512_stable,
+        target_feature = "avx512f",
+        target_feature = "avx512bw",
+        target_feature = "avx512vbmi",
+        target_feature = "avx512vbmi2"
+    )),
     not(target_feature = "avx2"),
     not(target_feature = "sse4.2")
 ))]
@@ -121,7 +278,17 @@ pub(crate) use super::validate_utf8_basic_fallback as validate_utf8_basic;
 
 // validate_utf8_compat() std: implementation auto-selection
 
-#[cfg(all(feature = "std", not(target_feature = "avx2")))]
+#[cfg(all(
+    feature = "std",
+    not(all(
+        avx512_stable,
+        target_feature = "avx512f",
+        target_feature = "avx512bw",
+        target_feature = "avx512vbmi",
+        target_feature = "avx512vbmi2"
+    )),
+    not(all(not(avx512_stable), target_feature = "avx2"))
+))]
 #[inline]
 pub(crate) unsafe fn validate_utf8_compat(
     input: &[u8],
@@ -133,14 +300,30 @@ pub(crate) unsafe fn validate_utf8_compat(
     type FnRaw = *mut ();
     type ValidateUtf8CompatFn = unsafe fn(input: &[u8]) -> Result<(), crate::compat::Utf8Error>;
 
+    #[cfg(avx512_stable)]
     #[inline]
-    fn get_fastest_available_implementation_compat() -> ValidateUtf8CompatFn {
+    fn get_avx512_implementation() -> Option<ValidateUtf8CompatFn> {
+        // Test for avx512vbmi2 to make sure we have a newer CPU with a non-throttling AVX-512 implementation
         if std::is_x86_feature_detected!("avx512f")
             && std::is_x86_feature_detected!("avx512bw")
             && std::is_x86_feature_detected!("avx512vbmi")
             && std::is_x86_feature_detected!("avx512vbmi2")
         {
-            avx512::validate_utf8_compat
+            return Some(avx512::validate_utf8_compat);
+        }
+        None
+    }
+
+    #[cfg(not(avx512_stable))]
+    #[inline]
+    fn get_avx512_implementation() -> Option<ValidateUtf8CompatFn> {
+        None
+    }
+
+    #[inline]
+    fn get_fastest_available_implementation_compat() -> ValidateUtf8CompatFn {
+        if let Some(fun) = get_avx512_implementation() {
+            fun
         } else if std::is_x86_feature_detected!("avx2") {
             avx2::validate_utf8_compat
         } else if std::is_x86_feature_detected!("sse4.2") {
@@ -168,7 +351,51 @@ pub(crate) unsafe fn validate_utf8_compat(
 
 // validate_utf8_basic() no-std: implementation selection by config
 
-#[cfg(target_feature = "avx2")]
+#[cfg(all(
+    avx512_stable,
+    target_feature = "avx512f",
+    target_feature = "avx512bw",
+    target_feature = "avx512vbmi",
+    target_feature = "avx512vbmi2"
+))]
+pub(crate) unsafe fn validate_utf8_compat(
+    input: &[u8],
+) -> core::result::Result<(), crate::compat::Utf8Error> {
+    if input.len() < super::helpers::SIMD_CHUNK_SIZE {
+        return super::validate_utf8_compat_fallback(input);
+    }
+
+    validate_utf8_compat_avx512(input)
+}
+
+#[cfg(all(
+    avx512_stable,
+    target_feature = "avx512f",
+    target_feature = "avx512bw",
+    target_feature = "avx512vbmi",
+    target_feature = "avx512vbmi2"
+))]
+#[inline(never)]
+unsafe fn validate_utf8_compat_avx512(
+    input: &[u8],
+) -> core::result::Result<(), crate::compat::Utf8Error> {
+    avx512::validate_utf8_compat(input)
+}
+
+#[cfg(any(
+    all(
+        not(feature = "std"),
+        not(all(
+            avx512_stable,
+            target_feature = "avx512f",
+            target_feature = "avx512bw",
+            target_feature = "avx512vbmi",
+            target_feature = "avx512vbmi2"
+        )),
+        target_feature = "avx2"
+    ),
+    all(target_feature = "avx2", feature = "std", not(avx512_stable))
+))]
 pub(crate) unsafe fn validate_utf8_compat(
     input: &[u8],
 ) -> core::result::Result<(), crate::compat::Utf8Error> {
@@ -179,7 +406,20 @@ pub(crate) unsafe fn validate_utf8_compat(
     validate_utf8_compat_avx2(input)
 }
 
-#[cfg(target_feature = "avx2")]
+#[cfg(any(
+    all(
+        not(feature = "std"),
+        not(all(
+            avx512_stable,
+            target_feature = "avx512f",
+            target_feature = "avx512bw",
+            target_feature = "avx512vbmi",
+            target_feature = "avx512vbmi2"
+        )),
+        target_feature = "avx2"
+    ),
+    all(target_feature = "avx2", feature = "std", not(avx512_stable))
+))]
 #[inline(never)]
 unsafe fn validate_utf8_compat_avx2(
     input: &[u8],
@@ -189,6 +429,13 @@ unsafe fn validate_utf8_compat_avx2(
 
 #[cfg(all(
     not(feature = "std"),
+    not(all(
+        avx512_stable,
+        target_feature = "avx512f",
+        target_feature = "avx512bw",
+        target_feature = "avx512vbmi",
+        target_feature = "avx512vbmi2"
+    )),
     not(target_feature = "avx2"),
     target_feature = "sse4.2"
 ))]
@@ -204,6 +451,13 @@ pub(crate) unsafe fn validate_utf8_compat(
 
 #[cfg(all(
     not(feature = "std"),
+    not(all(
+        avx512_stable,
+        target_feature = "avx512f",
+        target_feature = "avx512bw",
+        target_feature = "avx512vbmi",
+        target_feature = "avx512vbmi2"
+    )),
     not(target_feature = "avx2"),
     target_feature = "sse4.2"
 ))]
@@ -216,6 +470,13 @@ pub(crate) unsafe fn validate_utf8_compat_sse42(
 
 #[cfg(all(
     not(feature = "std"),
+    not(all(
+        avx512_stable,
+        target_feature = "avx512f",
+        target_feature = "avx512bw",
+        target_feature = "avx512vbmi",
+        target_feature = "avx512vbmi2"
+    )),
     not(target_feature = "avx2"),
     not(target_feature = "sse4.2")
 ))]
