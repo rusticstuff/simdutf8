8cda9bf0ca9f8c668f0ea50c11fd348d870f9211
Hans Kratz <hans@appfour.com>
2026-06-22 07:50:04 +0200

    update lib.rs from README

--- portable/src/lib.rs
+++ portable/src/lib.rs
@@ -12,29 +12,79 @@
 #![feature(doc_cfg)]
 #![feature(portable_simd)]
 
-//! Blazingly fast API-compatible UTF-8 validation for Rust using SIMD extensions, based on the implementation from
-//! [simdjson](https://github.com/simdjson/simdjson). Originally ported to Rust by the developers of [simd-json.rs](https://simd-json.rs), but now heavily improved.
+//! # simdutf8-portable – Fast UTF-8 validation using `core::simd` (portable SIMD)
+//!
+//! Fast API-compatible UTF-8 validation for Rust using the experimental
+//! [`core::simd`](https://doc.rust-lang.org/core/simd/index.html) (portable SIMD) module from the
+//! standard library. An up-to-date nightly Rust compiler is required. The API and the algorithm are the
+//! same as in the [simdutf8](https://crates.io/crates/simdutf8) crate.
+//!
+//! It is not planned to release this crate on crates.io until `core::simd` is stabilized.
+//!
+//! ## Features
+//!
+//! - no unsafe code (`#[forbid(unsafe_code)]`) in the implementation
+//! - good performance (with **caveats**, see [architecture notes](#Architecture-notes))
+//! - `auto` module which selects the best implementation for known-good targets at compile-time
+//!   including falling back to `core::str:from_utf8`.
+//! - new platforms need no new code as long as they are supported by `core::simd`
+//! - `no_std` support
+//! - supports 128-bit and 256-bit SIMD
+//!
+//! ## Limitations
+//!
+//! The functions in `core::simd` are marked as `#[inline]`. This way they are compiled to MIR Code generation is deferred until they are actually used in a crate.
+//! - target-feature required
+//! - no runtime implementation selection
+//! - slower
+//!   - memcpy calls
+//! - swizzle_dyn
+//!   - slow on non-special-cased targets
+//!   - requires -Zbuild-std for sse4.2, avx2 support if not part of the target architecture
+//!
+//! ## Performance
+//!
+//! - There are no unnecessary bounds checks in the compiled code, functions are properly inlined and
+//!   loops properly unrolled (as of nightly-xx).
+//! - uses memcpy because of forbid(unsafe), see https://github.com/llvm/llvm-project/issues/87440
+//! - swizzle_dyn slower because of abstraction
+//!
+//! ## Architecture notes
+//!
+//! | Architecture    | [Targets](https://doc.rust-lang.org/nightly/rustc/platform-support.html) | Performance\*   | Notes                                                                                                                                                                                                                                         |
+//! | --------------- | ------------------------------------------------------------------------ | --------------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+//! | aarch64         | all                                                                      | xx% of simdutf8 | works out of the box                                                                                                                                                                                                                          |
+//! | x86_64 (avx2)   | all                                                                      | xx% of simdutf8 | requires `-Zbuild-std` and `RUSTFLAGS="-C target-feature=+avx2"`                                                                                                                                                                              |
+//! | x86_64 (sse4.2) | all                                                                      | xx% of simdutf8 | requires `-Zbuild-std` and `RUSTFLAGS="-C target-feature=+sse4.2"`                                                                                                                                                                            |
+//! | wasm32          | all                                                                      | (not tested)    | requires `-Zbuild-std` and `RUSTFLAGS="-C target-feature=+simd128"`                                                                                                                                                                           |
+//! | armv7           | thumbv7neon&#x2011;\*                                                    | (not tested)    | works out of the box                                                                                                                                                                                                                          |
+//! | armv7           | others                                                                   | (not tested)    | requires `-Zbuild-std` and `RUSTFLAGS="-C target-feature=+neon"`                                                                                                                                                                              |
+//! | other           | ...                                                                      | bad             | falls back to `core::str::from_utf8` unless `forcesimd128` or `forcesimd256` are used. Check [`swizzle_dyn` support](https://github.com/rust-lang/rust/blob/master/library/portable-simd/crates/core_simd/src/swizzle_dyn.rs) before forcing. |
+//!
+//! \*) using `basic::from_utf8`
 //!
 //! ## Quick start
+//!
 //! Add the dependency to your Cargo.toml file:
+//!
 //! ```toml
 //! [dependencies]
-//! simdutf8 = "0.1.5"
+//! simdutf8-portable = { git = "https://github.com/rusticstuff/simdutf8" }
 //! ```
 //!
-//! Use [`basic::from_utf8()`] as a drop-in replacement for `std::str::from_utf8()`.
+//! Use `simdutf8-portable::basic::from_utf8()` as a drop-in replacement for `std::str::from_utf8()`.
 //!
 //! ```rust
-//! use simdutf8_portable::basic::from_utf8;
+//! use simdutf8-portable::basic::from_utf8;
 //!
 //! println!("{}", from_utf8(b"I \xE2\x9D\xA4\xEF\xB8\x8F UTF-8!").unwrap());
 //! ```
 //!
-//! If you need detailed information on validation failures, use [`compat::from_utf8()`]
+//! If you need detailed information on validation failures, use `simdutf8::compat::from_utf8()`
 //! instead.
 //!
 //! ```rust
-//! use simdutf8_portable::compat::from_utf8;
+//! use simdutf8-portable::compat::from_utf8;
 //!
 //! let err = from_utf8(b"I \xE2\x9D\xA4\xEF\xB8 UTF-8!").unwrap_err();
 //! assert_eq!(err.valid_up_to(), 5);
@@ -43,61 +93,36 @@
 //!
 //! ## APIs
 //!
-//! ### Basic flavor
-//! Use the `basic` API flavor for maximum speed. It is fastest on valid UTF-8, but only checks
-//! for errors after processing the whole byte sequence and does not provide detailed information if the data
-//! is not valid UTF-8. [`basic::Utf8Error`] is a zero-sized error struct.
-//!
-//! ### Compat flavor
-//! The `compat` flavor is fully API-compatible with `std::str::from_utf8()`. In particular, [`compat::from_utf8()`]
-//! returns a [`compat::Utf8Error`], which has [`valid_up_to()`](compat::Utf8Error#method.valid_up_to) and
-//! [`error_len()`](compat::Utf8Error#method.error_len) methods. The first is useful for verification of streamed data. The
-//! second is useful e.g. for replacing invalid byte sequences with a replacement character.
-//!
-//! It also fails early: errors are checked on the fly as the string is processed and once
-//! an invalid UTF-8 sequence is encountered, it returns without processing the rest of the data.
-//! This comes at a slight performance penalty compared to the [`basic`] API even if the input is valid UTF-8.
-//!
-//! ## Implementation selection
-//!
-//! ### X86
-//! The fastest implementation is selected at runtime using the `std::is_x86_feature_detected!` macro, unless the CPU
-//! targeted by the compiler supports the fastest available implementation.
-//! So if you compile with `RUSTFLAGS="-C target-cpu=native"` on a recent x86-64 machine, the AVX 2 implementation is selected at
-//! compile-time and runtime selection is disabled.
-//!
-//! For no-std support (compiled with `--no-default-features`) the implementation is always selected at compile time based on
-//! the targeted CPU. Use `RUSTFLAGS="-C target-feature=+avx2"` for the AVX 2 implementation or `RUSTFLAGS="-C target-feature=+sse4.2"`
-//! for the SSE 4.2 implementation.
-//!
-//! ### ARM64
-//! The SIMD implementation is used automatically since Rust 1.61.
-//!
-//! ### WASM32
-//! For wasm32 support, the implementation is selected at compile time based on the presence of the `simd128` target feature.
-//! Use `RUSTFLAGS="-C target-feature=+simd128"` to enable the WASM SIMD implementation.  WASM, at
-//! the time of this writing, doesn't have a way to detect SIMD through WASM itself.  Although this capability
-//! is available in various WASM host environments (e.g., [wasm-feature-detect] in the web browser), there is no portable
-//! way from within the library to detect this.
-//!
-//! [wasm-feature-detect]: https://github.com/GoogleChromeLabs/wasm-feature-detect
-//!
-//! ### Access to low-level functionality
-//! If you want to be able to call a SIMD implementation directly, use the `public_imp` feature flag. The validation
-//! implementations are then accessible via [`basic::imp`] and [`compat::imp`]. Traits facilitating streaming validation are available
-//! there as well.
-//!
-//! ## Optimisation flags
-//! Do not use [`opt-level = "z"`](https://doc.rust-lang.org/cargo/reference/profiles.html), which prevents inlining and makes
-//! the code quite slow.
+//! See docs for the main [simdutf8](https://crates.io/crates/simdutf8) crate.
 //!
 //! ## Minimum Supported Rust Version (MSRV)
-//! This crate's minimum supported Rust version is 1.38.0.
 //!
-//! ## Algorithm
+//! Rust nightly as of 2026-06-22.
 //!
-//! See Validating UTF-8 In Less Than One Instruction Per Byte, Software: Practice and Experience 51 (5), 2021
-//! <https://arxiv.org/abs/2010.03090>
+//! ## Thanks
+//!
+//! - to [Heinz N. Gies](https://github.com/licenser) for the initial portable SIMD implementation,
+//! - to the authors of `core::simd` module,
+//! - to the authors of simdjson for coming up with the high-performance SIMD implementation,
+//! - to the authors of the simdjson Rust port who did most of the initial heavy lifting of porting the
+//!   C++ code to Rust.
+//!
+//! ## License
+//!
+//! This code is dual-licensed under the
+//! [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0.html) and the
+//! [MIT License](https://opensource.org/licenses/MIT).
+//!
+//! It is based on code distributed with simd-json.rs, the Rust port of simdjson, which is dual-licensed
+//! under the MIT license and Apache 2.0 license as well.
+//!
+//! simdjson itself is distributed under the Apache License 2.0.
+//!
+//! ## References
+//!
+//! John Keiser, Daniel Lemire,
+//! [Validating UTF-8 In Less Than One Instruction Per Byte](https://arxiv.org/abs/2010.03090),
+//! Software: Practice and Experience 51 (5), 2021
 
 pub mod basic;
 pub mod compat;
